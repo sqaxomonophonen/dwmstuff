15fa4ec2c59a7ac4a97fa43cf92793f96f92189d
Anders Kaare <anders@straadt.dk>
2020-05-10 11:47:11 +0200

    better multiplexing
    process X events until it's time to "render the next tick". better than the
    previous solution where I just slept for 100ms always - it made both the render
    and the event handling a bit janky :)

--- rootwin/rootwin.c
+++ rootwin/rootwin.c
@@ -5,6 +5,9 @@
 #include <time.h>
 #include <math.h>
 #include <sys/select.h>
+#include <stdint.h>
+#include <sys/time.h>
+#include <assert.h>
 
 #include <X11/Xlib.h>
 #include <X11/XF86keysym.h>
@@ -42,12 +45,42 @@
 
 #define PSFIX "/sys/class/power_supply/"
 
-static void mysleep()
+static int wait_fd_seconds(int fd, double seconds)
 {
-	struct timeval timeout;
-	timeout.tv_sec = 0;
-	timeout.tv_usec = 100000;
-	select(0, NULL, NULL, NULL, &timeout);
+	struct timeval tv;
+	fd_set in_fds;
+	FD_ZERO(&in_fds);
+	FD_SET(fd, &in_fds);
+	tv.tv_sec = trunc(seconds);
+	tv.tv_usec = (seconds - trunc(seconds)) * 1e6;
+	return select(fd+1, &in_fds, 0, 0, &tv);
+}
+
+static int XNextEventTimeout(Display *display, XEvent *event, double seconds)
+{
+	if (XPending(display) || wait_fd_seconds(ConnectionNumber(display), seconds)) {
+		XNextEvent(display, event);
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+static int wait_for_timer(double* wait_seconds, uint64_t* tick)
+{
+	const int ticks_per_second = 3;
+	const uint64_t half_tick_usec = 1000000 / (ticks_per_second * 2);
+
+	struct timeval tv;
+	assert(gettimeofday(&tv, NULL) == 0);
+	uint64_t current_tick = (tv.tv_sec * (ticks_per_second*2) + (tv.tv_usec / half_tick_usec) + 1) / 2;
+	if (current_tick == *tick) {
+		*wait_seconds = ((double)(half_tick_usec - (tv.tv_usec % half_tick_usec))) / 1e6;
+		return 1;
+	} else {
+		*tick = current_tick;
+		return 0;
+	}
 }
 
 int main(int argc, char** argv)
@@ -81,13 +114,64 @@ int main(int argc, char** argv)
 	XGrabKey(dpy, XKeysymToKeycode(dpy, XF86XK_MonBrightnessDown), 0, root, False, GrabModeAsync, GrabModeAsync);
 	XGrabKey(dpy, XKeysymToKeycode(dpy, XF86XK_MonBrightnessUp), 0, root, False, GrabModeAsync, GrabModeAsync);
 
-	int i = 0;
-	for (;;i++) {
+	uint64_t tick = 0;
+	for (;;) {
+		//printf("tick %ld\n", tick);
 		char buf[SZ];
 		char* bufptr = buf;
 		size_t buf_remaining = SZ;
 
 		char tmp[SZ];
+
+		for (;;) {
+			double wait_seconds;
+			if (!wait_for_timer(&wait_seconds, &tick)) break;
+
+			XEvent ev;
+			if (XNextEventTimeout(dpy, &ev, wait_seconds)) {
+				KeySym sym;
+				double brightness_delta = 0;
+				switch (ev.type) {
+				case KeyPress:
+					sym = XLookupKeysym(&ev.xkey, 0);
+					if (sym == XF86XK_MonBrightnessDown) {
+						brightness_delta = -1;
+					} else if (sym == XF86XK_MonBrightnessUp) {
+						brightness_delta = 1;
+					}
+					break;
+				}
+
+				if (brightness_delta != 0) {
+					READTMP("/sys/class/backlight/intel_backlight/max_brightness");
+					double max_brightness = strtod(tmp, NULL);
+
+					const char* brightness_path = "/sys/class/backlight/intel_backlight/brightness";
+					READTMP(brightness_path);
+					double brightness = strtod(tmp, NULL);
+
+					const double power = 2.4;
+					const double step = 0.015;
+
+					double new_brightness = pow(pow(brightness / max_brightness, 1/power) + brightness_delta*step, power) * max_brightness;
+					if (new_brightness > max_brightness) new_brightness = max_brightness;
+					if (!isfinite(new_brightness)) new_brightness = 0;
+					if (new_brightness < 1) new_brightness = 1;
+
+					FILE* f = fopen(brightness_path, "w");
+					if (f != NULL) {
+						//printf("%d -> %s\n", (int)new_brightness, brightness_path);
+						fprintf(f, "%d\n", (int)new_brightness);
+						fclose(f);
+					} else {
+						fprintf(stderr, "%s: could not write\n", brightness_path);
+					}
+				}
+			}
+
+
+		}
+
 
 		{
 			READTMP("/proc/loadavg");
@@ -118,7 +202,7 @@ int main(int argc, char** argv)
 				sep = ':';
 			}
 
-			if (pct < 10 && i&1 && discharging) {
+			if (pct < 10 && tick&1 && discharging) {
 				BUFN("    ");
 			} else {
 				BUFN("B%c%d%%", sep, pct);
@@ -140,53 +224,9 @@ int main(int argc, char** argv)
 			BUFN("%s", tmp);
 		}
 
-		while (XPending(dpy)) {
-			XEvent ev;
-			XNextEvent(dpy, &ev);
-			KeySym sym;
-			double brightness_delta = 0;
-			switch (ev.type) {
-			case KeyPress:
-				sym = XLookupKeysym(&ev.xkey, 0);
-				if (sym == XF86XK_MonBrightnessDown) {
-					brightness_delta = -1;
-				} else if (sym == XF86XK_MonBrightnessUp) {
-					brightness_delta = 1;
-				}
-				break;
-			}
-
-			if (brightness_delta != 0) {
-				READTMP("/sys/class/backlight/intel_backlight/max_brightness");
-				double max_brightness = strtod(tmp, NULL);
-
-				const char* brightness_path = "/sys/class/backlight/intel_backlight/brightness";
-				READTMP(brightness_path);
-				double brightness = strtod(tmp, NULL);
-
-				const double power = 2.4;
-				const double step = 0.015;
-
-				double new_brightness = pow(pow(brightness / max_brightness, 1/power) + brightness_delta*step, power) * max_brightness;
-				if (new_brightness > max_brightness) new_brightness = max_brightness;
-				if (!isfinite(new_brightness)) new_brightness = 0;
-				if (new_brightness < 1) new_brightness = 1;
-
-				FILE* f = fopen(brightness_path, "w");
-				if (f != NULL) {
-					//printf("%d -> %s\n", (int)new_brightness, brightness_path);
-					fprintf(f, "%d\n", (int)new_brightness);
-					fclose(f);
-				} else {
-					fprintf(stderr, "%s: could not write\n", brightness_path);
-				}
-			}
-		}
 
 		XStoreName(dpy, root, buf);
 		XSync(dpy, False);
-
-		mysleep();
 	}
 
 	XCloseDisplay(dpy);
